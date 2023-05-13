1bf43b3823bc78d43f52dd8012be4565e11b6694
Oblong101 <aaron.hughes0707@gmail.com>
2023-05-13 02:03:45 +0100

    added more files!!!

--- src/main.py
+++ src/main.py
@@ -2,3 +2,13 @@ import disnake
 from disnake.ext import commands
 
 
+def bot():
+    bot = commands.InteractionBot(intents=disnake.Intents.)
+
+    """
+    Every cog should have its path added to this list relative to this file
+    Each item should be separated by full stops, eg. 'cogs.tools.x'
+    """
+    cog_list = []
+
+
