dc6d9c4e601aac7421e7ecc8af0f2f0109bedc0e
Oblong101 <aaron.hughes0707@gmail.com>
2023-05-13 02:04:24 +0100

    config and stuff

---
+++ src/config.py
@@ -0,0 +1,6 @@
+"""
+This file should contain constant values like your bot token (stored in the BOT_TOKEN variable),
+and different channel IDs which can be called when needed.
+"""
+
+BOT_TOKEN = ""
