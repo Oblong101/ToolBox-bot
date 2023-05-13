eefb478d77f44729e08502bc63281f7216be063b
Oblong <121264821+Oblong101@users.noreply.github.com>
2023-05-13 23:16:45 +0100

    Delete config.py

--- src/config.py
+++
@@ -1,6 +0,0 @@
-"""
-This file should contain constant values like your bot token (stored in the BOT_TOKEN variable),
-and different channel IDs which can be called when needed.
-"""
-
-BOT_TOKEN = "MTEwNjc0NzI4MTAwNjQxNTkwMw.GzI0rV.91hLNwOW20dsmLrOqeAP9-NRWYLV3CH0qxzTlk"
